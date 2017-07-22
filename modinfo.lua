bb83d7fea88696d4e0a1571e03312c4050f4f5d7
jilin <smith3816@gmail.com>
2017-07-22 14:00:25 +0800

    updage logo

--- modinfo.lua
+++ modinfo.lua
@@ -2,19 +2,19 @@ local descList =
 {
 	"Provide additional items for game. Enjoy your new package! (You can close part of package in options)",
 	"提供额外的物品合成，享受更加丰富的游戏内容吧！（您可以在选项中选择关闭不需要的内容）",
-	"Item list:",
-	"物品列表：",
-	"- Popcorn Gun",
-	"- 玉米枪",
-	"- incinerator",
-	"- 焚烧炉",
 
-	"\n\nFood Recipes:",
-	"食品配方：",
-	"egg pancake (egg >= 3 and not inedible)",
-	"鸡蛋灌饼",
+	"\nView Steam workshop for more info",
+	"游览Steam创意工坊查看更多信息",
 
-	"\n\n新浪微博：@二货爱吃白萝卜",
+	"\n新浪微博：@二货爱吃白萝卜",
+	--[["Weapon: Popcorn Gun",
+	"武器: 玉米枪",
+
+	"Building: incinerator",
+	"建筑： 焚烧炉",
+
+	"Food Recipes: Egg Pancake, Monster Salad, Skunk Smoothies, Fish Froggle, Bamboo Light, Vegetaballs, Veggie Lohan, Honey Drumstick, Meat Burger, Veggie Skewers, Stinky Mandarin Fish, Watermelon Juice, Caterpillar Bread, Durian Sugar, Frozen Heart",
+	"食品配方： 鸡蛋灌饼，怪物沙拉，臭鼬果沙，美蛙鱼头，星光特典，蔬菜丸子，素罗汉，蜜汁鸡腿，混合肉堡，素食串，臭鲑鱼，西瓜汁，毛毛虫，榴莲糖，冰冻之心",]]
 }
 
 local function joinArray(arr, spliter)
