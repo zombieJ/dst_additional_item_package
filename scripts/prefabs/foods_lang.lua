a481692357fd883bb9282ec8ee3ce167cc2cca4a
Artyom Egorkin <30765190+MagVI@users.noreply.github.com>
2017-09-20 17:38:32 +0300

    rus lang

--- scripts/prefabs/foods_lang.lua
+++ scripts/prefabs/foods_lang.lua
@@ -187,6 +187,10 @@ local LANG_MAP = {
 		["FROZEN_HEART"] = {
 			["NAME"] = "Ледяное Сердце",
 			["DESC"] = "Почему я могу сделать это в казане?",
+		},	
+		["AIP_FOOD_EGG_FRIED_RICE"] = {
+			["NAME"] = "Рис с жаренным яйцом",
+			["DESC"] = "Пахнет вкусно!",
 		},
 	},
 	["portuguese"] = {
