a1e00219749f40754feea7d53c3835f35b7033a6
jilin <smith3816@gmail.com>
2017-12-18 23:20:37 +0800

    add obit item

--- scripts/prefabs/aip_orbit.lua
+++ scripts/prefabs/aip_orbit.lua
@@ -28,6 +28,7 @@ local LANG = LANG_MAP[language] or LANG_MAP.english
 local assets =
 {
 	Asset("ATLAS", "images/inventoryimages/aip_orbit.xml"),
+	Asset("ATLAS", "images/inventoryimages/aip_orbit_item.xml"),
 	Asset("ANIM", "anim/aip_orbit.zip"),
 }
 
@@ -36,16 +37,130 @@ local prefabs =
 }
 
 -- 文字描述
+STRINGS.NAMES.AIP_ORBIT_ITEM = LANG.NAME
+STRINGS.RECIPE_DESC.AIP_ORBIT_ITEM = LANG.REC_DESC
 STRINGS.NAMES.AIP_ORBIT = LANG.NAME
-STRINGS.RECIPE_DESC.AIP_ORBIT = LANG.REC_DESC
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.AIP_ORBIT = LANG.DESC
 
 -- 配方
-local aip_orbit = Recipe("aip_orbit", {Ingredient("boards", 1)}, RECIPETABS.TOWN, TECH.SCIENCE_ONE, "aip_orbit_placer")
-aip_orbit.atlas = "images/inventoryimages/aip_orbit.xml"
+local aip_orbit_item = Recipe("aip_orbit_item", {Ingredient("boards", 1)}, RECIPETABS.TOWN, TECH.SCIENCE_ONE, nil, nil, nil, 4)
+aip_orbit_item.atlas = "images/inventoryimages/aip_orbit_item.xml"
+
+--------------------------- 轨道物品 ---------------------------
+local function ondeploywall(inst, pt, deployer)
+	local wall = SpawnPrefab("aip_orbit")
+	if wall ~= nil then 
+		local x = math.floor(pt.x) + .5
+		local z = math.floor(pt.z) + .5
+		wall.Physics:SetCollides(false)
+		wall.Physics:Teleport(x, 0, z)
+		-- wall.Physics:SetCollides(true)
+		inst.components.stackable:Get():Remove()
+		
+		wall.SoundEmitter:PlaySound("dontstarve/common/place_structure_wood")
+	end
+end
+
+local function itemfn()
+	local inst = CreateEntity()
+
+	inst.entity:AddTransform()
+	inst.entity:AddAnimState()
+	inst.entity:AddNetwork()
+
+	MakeInventoryPhysics(inst)
+
+	inst:AddTag("wallbuilder")
+
+	inst.AnimState:SetBank("aip_orbit")
+	inst.AnimState:SetBuild("aip_orbit")
+	inst.AnimState:PlayAnimation("idle")
+
+	inst.entity:SetPristine()
+
+	if not TheWorld.ismastersim then
+		return inst
+	end
+
+	inst:AddComponent("stackable")
+	inst.components.stackable.maxsize = TUNING.STACK_SIZE_MEDITEM
+
+	inst:AddComponent("inspectable")
+	inst:AddComponent("inventoryitem")
+	inst.components.inventoryitem.atlasname = "images/inventoryimages/aip_orbit_item.xml"
+
+	inst:AddComponent("deployable")
+	inst.components.deployable.ondeploy = ondeploywall
+	inst.components.deployable:SetDeployMode(DEPLOYMODE.WALL)
+
+	MakeHauntableLaunch(inst)
+
+	return inst
+end
+
+----------------------------- 轨道 -----------------------------
+local function onhammered(inst, worker)
+	if inst.components.burnable ~= nil and inst.components.burnable:IsBurning() then
+		inst.components.burnable:Extinguish()
+	end
+	inst.components.lootdropper:DropLoot()
+	local fx = SpawnPrefab("collapse_small")
+	fx.Transform:SetPosition(inst.Transform:GetWorldPosition())
+	fx:SetMaterial("wood")
+	inst:Remove()
+end
+
+local function onhit(inst, worker)
+	if not inst:HasTag("burnt") then
+		inst.AnimState:PlayAnimation("hit")
+		inst.AnimState:PushAnimation("idle", false)
+	end
+end
+
+local function fn()
+	local inst = CreateEntity()
+
+	inst.entity:AddTransform()
+	inst.entity:AddAnimState()
+	inst.entity:AddSoundEmitter()
+	inst.entity:AddNetwork()
+
+	inst.Transform:SetEightFaced()
+
+	MakeObstaclePhysics(inst, .5)
+	inst.Physics:SetDontRemoveOnSleep(true)
+
+	inst:AddTag("wall")
+	inst:AddTag("noauradamage")
+	inst:AddTag("nointerpolate")
+	inst:AddTag("wood")
+
+	inst.AnimState:SetBank("aip_orbit")
+	inst.AnimState:SetBuild("aip_orbit")
+	inst.AnimState:PlayAnimation("idle")
+
+	inst.entity:SetPristine()
+
+	if not TheWorld.ismastersim then
+		return inst
+	end
+
+	inst:AddComponent("inspectable")
+	inst:AddComponent("lootdropper")
+
+	inst:AddComponent("workable")
+	inst.components.workable:SetWorkAction(ACTIONS.HAMMER)
+	inst.components.workable:SetWorkLeft(3)
+	inst.components.workable:SetOnFinishCallback(onhammered)
+	inst.components.workable:SetOnWorkCallback(onhit)
+
+	MakeHauntableWork(inst)
+
+	return inst
+end
 
 -----------------------------------------------------------
-local function onhammered(inst, worker)
+--[[local function onhammered(inst, worker)
 	if inst.components.burnable ~= nil and inst.components.burnable:IsBurning() then
 		inst.components.burnable:Extinguish()
 	end
@@ -76,7 +191,7 @@ function fn()
 	inst.entity:AddMiniMapEntity()
 	inst.entity:AddNetwork()
 
-	MakeObstaclePhysics(inst, 0)
+	MakeInventoryPhysics(inst)
 
 	inst.AnimState:SetBank("aip_orbit")
 	inst.AnimState:SetBuild("aip_orbit")
@@ -107,7 +222,8 @@ function fn()
 	inst:ListenForEvent("onbuilt", onbuilt)
 
 	return inst
-end
+end]]
 
 return Prefab("aip_orbit", fn, assets, prefabs),
-	MakePlacer("aip_orbit_placer", "aip_orbit", "aip_orbit", "idle", nil, nil, nil, nil, -90, "eight")
+	Prefab("aip_orbit_item", itemfn, assets, { "aip_orbit", "aip_orbit_item_placer" }),
+	MakePlacer("aip_orbit_item_placer", "aip_orbit", "aip_orbit", "idle", false, false, true, nil, -90, "eight")
