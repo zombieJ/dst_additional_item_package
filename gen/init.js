feae25d24417462933e35068270bdc6441f8a7e8
zombieJ <smith3816@gmail.com>
2019-06-25 00:35:28 +0800

    auto gen!

--- gen/init.js
+++ gen/init.js
@@ -31,20 +31,40 @@ function run() {
   fs.removeSync(tmpPath);
   fs.ensureDirSync(tmpPath);
 
-  fs.copySync(path.resolve(prjPath, 'exported'), path.resolve(tmpPath, 'exported'));
-  fs.copySync(path.resolve(prjPath, 'exported_done'), path.resolve(tmpPath, 'exported_done'));
+  fs.copySync(
+    path.resolve(prjPath, 'exported'),
+    path.resolve(tmpPath, 'exported')
+  );
+  fs.copySync(
+    path.resolve(prjPath, 'exported_done'),
+    path.resolve(tmpPath, 'exported_done')
+  );
 
-  // ================ Move all exists files to exported ================
-  const fileList = fs.readdirSync(path.resolve(prjPath, 'exported_done'));
-  fileList.forEach(fileName => {
-    fs.copySync(path.resolve(prjPath, 'exported_done', fileName), path.resolve(prjPath, 'exported', fileName));
-  });
+  try {
+    // =============== Move all exists files to exported ===============
+    const fileList = fs.readdirSync(path.resolve(prjPath, 'exported_done'));
+    fileList.forEach(fileName => {
+      fs.copySync(
+        path.resolve(prjPath, 'exported_done', fileName),
+        path.resolve(prjPath, 'exported', fileName)
+      );
+    });
 
-  // ========================== Execute Tools ==========================
-  spawnSync(toolPath, {
-    stdio: [null, process.stdout, process.stderr]
-  });
-  console.log(chalk.green('Done!'));
+    // ========================= Execute Tools =========================
+    spawnSync(toolPath, {
+      stdio: [null, process.stdout, process.stderr]
+    });
+    console.log(chalk.green('Done!'));
+  } catch (err) {
+    console.error(err);
+  } finally {
+    console.log(chalk.cyan('Restore files'));
+    fs.removeSync(path.resolve(prjPath, 'exported'));
+    fs.moveSync(
+      path.resolve(tmpPath, 'exported'),
+      path.resolve(prjPath, 'exported')
+    );
+  }
 }
 
 run();
