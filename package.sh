aeb165d33a12570e400b0ef54b59588df8ae15ae
jilin <smith3816@gmail.com>
2019-10-01 20:39:05 +0800

    update package script

--- package.sh
+++ package.sh
@@ -1,7 +1,11 @@
+echo "Clean up..."
 rm -rf package/
+
+echo "Create related folder..."
 mkdir -p package/images/inventoryimages
 mkdir -p package/minimap
 
+echo "Copy resourse..."
 cp -r anim package/
 cp -r images/inventoryimages/*.tex package/images/inventoryimages/
 cp -r images/inventoryimages/*.xml package/images/inventoryimages/
@@ -13,6 +17,24 @@ cp modicon.xml package/
 cp modinfo.lua package/
 cp modmain.lua package/
 
+echo "Replace DEV mark..."
 sed -i 's/"(DEV MODE)",//g' package/modinfo.lua
 sed -i 's/name = "Additional Item Package DEV"/name = "Additional Item Package"/g' package/modinfo.lua
 sed -i 's/TUNING.ZOMBIEJ_ADDTIONAL_PACKAGE = "Additional Item Package DEV"/TUNING.ZOMBIEJ_ADDTIONAL_PACKAGE = "Additional Item Package"/g' package/modmain.lua
+
+if [[ -z $1 ]]; then
+	echo "Done! You can add additional folder path to copy the tmp folder out."
+else
+	echo "Copy generated package to $1"
+
+	echo " - cleanup..."
+	rm -rf $1
+
+	echo " - copy..."
+	cp -r package $1
+
+	echo " - add name mark..."
+	sed -i 's/name = "Additional Item Package"/name = "Additional Item Package (output)"/g' $1/modinfo.lua
+
+	echo " - done!"
+fi
