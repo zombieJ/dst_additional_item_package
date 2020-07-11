a96a96c0f1f6f175d32fe24abcd56b8445ed1e81
jilin <smith3816@gmail.com>
2020-07-11 23:11:14 +0800

    update script

--- dist.js
+++ dist.js
@@ -1,5 +1,7 @@
 const chalk = require('chalk');
 const fs = require('fs-extra');
+const path = require('path');
+const { argv } = require('yargs');
 
 console.log(chalk.yellow("Clean up..."));
 fs.removeSync('package/');
@@ -31,5 +33,29 @@ fs.copySync('modicon.xml', 'package/modicon.xml');
 fs.copySync('modinfo.lua', 'package/modinfo.lua');
 fs.copySync('modmain.lua', 'package/modmain.lua');
 console.log(chalk.green("Copy resourse...mode info done"));
+
+console.log(chalk.cyan("Replace DEV mark..."));
+function replaceText(filepath, src, tgt) {
+	let text = fs.readFileSync(filepath, 'utf8');
+	text = text.replace(src, tgt);
+	fs.writeFileSync(filepath, text, 'utf8');
+}
+replaceText('package/modinfo.lua', /"\(DEV MODE\)",/g, '');
+replaceText('package/modinfo.lua', /name = "Additional Item Package DEV"/g, 'name = "Additional Item Package"');
+replaceText('package/modmain.lua', /TUNING.ZOMBIEJ_ADDTIONAL_PACKAGE = "Additional Item Package DEV"/g, 'TUNING.ZOMBIEJ_ADDTIONAL_PACKAGE = "Additional Item Package"');
+
+if (argv.target) {
+	const relativePath = path.resolve(argv.target);
+	console.log(chalk.green(`Copy generated package to '${relativePath}'`));
+
+	console.log(chalk.yellow("Clean up..."));
+	fs.removeSync(relativePath);
+
+	console.log(chalk.cyan("Copy package..."));
+	fs.copySync('package', relativePath);
+
+	console.log(chalk.cyan("Add name mark..."));
+	replaceText(path.join(relativePath, '/modinfo.lua'), /name = "Additional Item Package"/g, 'name = "Additional Item Package (output)"');
+}
 
 console.log(chalk.green("All finished!"));
