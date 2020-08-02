a97fd3f7123692d8354837487e2bf0fe3a560666
jilin <smith3816@gmail.com>
2020-08-02 17:38:36 +0800

    inscriptions

--- scripts/prefabs/aip_dou_inscription.lua
+++ scripts/prefabs/aip_dou_inscription.lua
@@ -8,76 +8,107 @@ end
 local language = aipGetModConfig("language")
 
 local LANG_MAP = {
-	["english"] = {
-		["NAME"] = "Inscription",
-		["REC_DESC"] = "Alien from the sky",
-		["DESC"] = "Decorate your Walking Cane",
+	english = {
+		aip_dou_fire_inscription = {
+			NAME = "铭文",
+			REC_DESC = "火元素效果",
+			DESC = "用它来进行附魔",
+		},
+		aip_dou_ice_inscription = {
+			NAME = "铭文：火",
+			REC_DESC = "附魔火元素",
+			DESC = "它能够召唤火元素",
+		},
 	},
-	["chinese"] = {
-		["NAME"] = "铭文",
-		["REC_DESC"] = "火元素效果",
-		["DESC"] = "用它来进行附魔",
+	chinese = {
+		aip_dou_fire_inscription = {
+			NAME = "铭文：火",
+			REC_DESC = "附魔火元素",
+			DESC = "它能够召唤火元素",
+		},
+		aip_dou_ice_inscription = {
+			NAME = "铭文：冰",
+			REC_DESC = "附魔冰元素",
+			DESC = "它能够召唤冰元素",
+		},
 	},
 }
 
 local LANG = LANG_MAP[language] or LANG_MAP.english
 
--- 资源
-local assets =
-{
-	Asset("ATLAS", "images/inventoryimages/aip_dou_opal.xml"),
-	Asset("ANIM", "anim/aip_dou_opal.zip"),
+------------------------------------ 元素 ------------------------------------
+local INSCRIPTIONS = {
+	aip_dou_fire_inscription = { recipes = {Ingredient("log", 1)} },
+	aip_dou_ice_inscription = { recipes = {Ingredient("log", 1)} },
 }
-
-local prefabs =
-{
-}
-
--- 文字描述
-STRINGS.NAMES.AIP_DOU_INSCRIPTION = LANG.NAME
-STRINGS.RECIPE_DESC.AIP_DOU_INSCRIPTION = LANG.REC_DESC
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.AIP_DOU_INSCRIPTION = LANG.DESC
-
------------------------------------- 配方 ------------------------------------
-local aip_dou_inscription = Recipe("aip_dou_inscription", {Ingredient("log", 1)}, RECIPETABS.AIP_DOU_SCEPTER, TECH.AIP_DOU_SCEPTER_ONE, nil, nil, true)
-aip_dou_inscription.atlas = "images/inventoryimages/aip_dou_opal.xml"
-aip_dou_inscription.image = "aip_dou_opal.tex"
 
 ------------------------------------ 功能 ------------------------------------
-function fn()
-	local inst = CreateEntity()
+local function makeInscription(name, info)
+	-- 资源
+	local assets =
+	{
+		Asset("ATLAS", "images/inventoryimages/aip_dou_opal.xml"),
+		Asset("ANIM", "anim/aip_dou_opal.zip"),
+	}
 
-	inst.entity:AddTransform()
-	inst.entity:AddAnimState()
-	inst.entity:AddNetwork()
-	
-	MakeInventoryPhysics(inst)
+	-- 文字
+	local upperName = string.upper(name)
+	local PREFAB_LANG = LANG[name] or LANG_MAP.english[name]
 
-	inst.AnimState:SetBank("aip_dou_opal")
-	inst.AnimState:SetBuild("aip_dou_opal")
-	inst.AnimState:PlayAnimation("idle")
+	STRINGS.NAMES[upperName] = PREFAB_LANG.NAME
+	STRINGS.RECIPE_DESC[upperName] = PREFAB_LANG.REC_DESC
+	STRINGS.CHARACTERS.GENERIC.DESCRIBE[upperName] = PREFAB_LANG.DESC
 
-	MakeInventoryFloatable(inst, "med", 0.1, 0.75)
+	-- 生成配方
+	local aip_dou_inscription = Recipe(name, info.recipes, RECIPETABS.AIP_DOU_SCEPTER, TECH.AIP_DOU_SCEPTER_ONE, nil, nil, true)
+	aip_dou_inscription.atlas = "images/inventoryimages/aip_dou_opal.xml"
+	aip_dou_inscription.image = "aip_dou_opal.tex"
 
-	inst.entity:SetPristine()
+	local function fn()
+		local inst = CreateEntity()
 
-	if not TheWorld.ismastersim then
+		inst.entity:AddTransform()
+		inst.entity:AddAnimState()
+		inst.entity:AddNetwork()
+		
+		MakeInventoryPhysics(inst)
+
+		inst:AddTag("aip_dou_inscription")
+
+		inst.AnimState:SetBank("aip_dou_opal")
+		inst.AnimState:SetBuild("aip_dou_opal")
+		inst.AnimState:PlayAnimation("idle")
+
+		MakeInventoryFloatable(inst, "med", 0.1, 0.75)
+
+		inst.entity:SetPristine()
+
+		if not TheWorld.ismastersim then
+			return inst
+		end
+
+		inst:AddComponent("inspectable")
+
+		inst:AddComponent("inventoryitem")
+		inst.components.inventoryitem.atlasname = "images/inventoryimages/aip_dou_opal.xml"
+		inst.components.inventoryitem.imagename = "aip_dou_opal"
+
+		MakeSmallBurnable(inst, TUNING.LARGE_BURNTIME)
+		MakeSmallPropagator(inst)
+
+		MakeHauntableLaunchAndIgnite(inst)
+
 		return inst
 	end
 
-	-- 如果被雷击
-	inst:AddComponent("inspectable")
-
-	inst:AddComponent("inventoryitem")
-	inst.components.inventoryitem.atlasname = "images/inventoryimages/aip_dou_opal.xml"
-	inst.components.inventoryitem.imagename = "aip_dou_opal"
-
-	MakeSmallBurnable(inst, TUNING.LARGE_BURNTIME)
-	MakeSmallPropagator(inst)
-
-	MakeHauntableLaunchAndIgnite(inst)
-
-	return inst
+	return Prefab(name, fn, assets)
 end
 
-return Prefab("aip_dou_inscription", fn, assets, prefabs)
+-- 生成配方
+local inscriptions = {}
+
+for name, info in pairs(INSCRIPTIONS) do
+	table.insert(inscriptions, makeInscription(name, info))
+end
+
+return unpack(inscriptions)
