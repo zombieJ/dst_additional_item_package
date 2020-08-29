0a2c49f7f0031d72cf44ab5be21ff4a134a6f837
jilin <smith3816@gmail.com>
2020-08-30 00:50:08 +0800

    adjust projectile color

--- scripts/prefabs/aip_dou_scepter_projectile.lua
+++ scripts/prefabs/aip_dou_scepter_projectile.lua
@@ -32,10 +32,13 @@ local function fn()
     inst.AnimState:SetBank("aip_dou_scepter_projectile")
     inst.AnimState:SetBuild("aip_dou_scepter_projectile")
     inst.AnimState:PlayAnimation("idle", true)
+    inst.AnimState:OverrideMultColour(1, 0.8, 0, 1)
     -- inst.AnimState:SetBloomEffectHandle("shaders/anim.ksh")
 
     --projectile (from projectile component) added to pristine state for optimization
     inst:AddTag("projectile")
+    inst:AddTag("flying")
+    inst:AddTag("ignorewalkableplatformdrowning")
 
     inst.entity:SetPristine()
 
