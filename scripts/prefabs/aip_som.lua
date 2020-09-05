04922957ad18445185e377b4d537c6652d33e1b9
二货机器人 <smith3816@gmail.com>
2020-09-05 16:38:51 +0800

    feat: Ehancement of exist items (#38)
    * chore: Use cal damge
    
    * ocean fish sword
    
    * ocean it
    
    * more effect
    
    * add more func
    
    * aura it
    
    * aura
    
    * popcorngun recpie
    
    * no vest
    
    * dist generate zip file
    
    * fix inst logic
    
    * clean up

--- scripts/prefabs/aip_som.lua
+++ scripts/prefabs/aip_som.lua
@@ -33,15 +33,34 @@ STRINGS.NAMES.AIP_SOM = LANG.NAME
 STRINGS.RECIPE_DESC.AIP_SOM = LANG.REC_DESC
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.AIP_SOM = LANG.DESC
 
--- 配方
--- local aip_horse_head = Recipe("aip_horse_head", {Ingredient("beefalowool", 5),Ingredient("boneshard", 3),Ingredient("beardhair", 3)}, RECIPETABS.DRESS, TECH.SCIENCE_TWO)
--- aip_horse_head.atlas = "images/inventoryimages/aip_horse_head.xml"
+local refreshInterval = 0.1
 
 local tempalte = require("prefabs/aip_dress_template")
 return tempalte("aip_som", {
+	prefabs = { "aip_vest" },
 	hideHead = true,
 	fueled = {
 		level = TUNING.AIP_SOM_FUEL,
 	},
-	dapperness = TUNING.DAPPERNESS_LARGE,
+	dapperness = TUNING.DAPPERNESS_TINY,
+
+	onEquip = function(inst, owner)
+		-- 添加光环
+		local follower = SpawnPrefab("aip_vest")
+
+		follower:AddComponent("sanityaura")
+		follower.components.sanityaura.aura = TUNING.DAPPERNESS_LARGE
+
+		inst._follower = follower
+
+		inst._auraTask = inst:DoPeriodicTask(refreshInterval, function()
+			follower.Transform:SetPosition(owner.Transform:GetWorldPosition())
+		end)
+	end,
+	onUnequip = function(inst, owner)
+		-- 移除光环
+		inst._auraTask:Cancel()
+		inst._auraTask = nil
+		inst._follower:Remove()
+	end,
 })
---
+++ scripts/prefabs/aip_vest.lua
@@ -0,0 +1,33 @@
+-- 马甲单位
+local assets =
+{
+	Asset("ANIM", "anim/staff_projectile.zip"),
+}
+
+function fn()
+	local inst = CreateEntity()
+
+	inst.entity:AddTransform()
+	inst.entity:AddAnimState()
+	inst.entity:AddNetwork()
+
+	MakeInventoryPhysics(inst)
+	RemovePhysicsColliders(inst)
+
+	inst.entity:SetPristine()
+
+	if not TheWorld.ismastersim then
+		return inst
+	end
+
+	inst.persists = false
+
+	-- 马甲不过夜
+	inst.OnLoad = function()
+		inst:Remove()
+	end
+
+	return inst
+end
+
+return Prefab("aip_vest", fn, assets)
