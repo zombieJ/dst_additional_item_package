84cd8393de0fc8139c5eed828883dd52b5eec2d4
jilin <smith3816@gmail.com>
2020-10-04 13:50:48 +0800

    adjust speed

--- scripts/aipUtils.lua
+++ scripts/aipUtils.lua
@@ -133,7 +133,7 @@ end
 -- 返回角度：0 ~ 360
 function GLOBAL.aipGetAngle(src, tgt)
 	local direction = (tgt - src):GetNormalized()
-	local angle = math.acos(direction:Dot(GLOBAL.Vector3(1, 0, 0))) / DEGREES
+	local angle = math.acos(direction:Dot(GLOBAL.Vector3(1, 0, 0))) / GLOBAL.DEGREES
 	if direction.z < 0 then
 		angle = 360 - angle
 	end
--- scripts/components/aipc_guardian_orb.lua
+++ scripts/components/aipc_guardian_orb.lua
@@ -1,7 +1,10 @@
-local ANGLE_SPEED = 5
+local ORB_SPEED = 9
+local ORB_SPEED_FAST = 20
+local ANGLE_SPEED = 360
 local MAX_COUNT = 3
 local INCREASE_TIMEOUT = 2
-local DISTANCE = 1
+local DISTANCE = 1.5
+local DISTANCE_FAST = 2
 
 local function getTargetPoint(angle, pos, offset)
 	local offsetAngle = angle + (360 / MAX_COUNT * offset)
@@ -21,8 +24,6 @@ local GuardianOrb = Class(function(self, inst)
 	self.timeout = 0
 	-- 召唤的法球物质
 	self.spawnPrefab = nil
-	-- 初始化速度
-	self.speed = 20
 end)
 
 -- 启动守护法球
@@ -49,7 +50,7 @@ function GuardianOrb:OnUpdate(dt)
 			local orb = SpawnPrefab(self.spawnPrefab)
 			table.insert(self.orbs, orb)
 			orb.Transform:SetPosition(instPos.x, instPos.y, instPos.z)
-			orb.Physics:SetMotorVel(self.speed, 0, 0)
+			orb.Physics:SetMotorVel(ORB_SPEED, 0, 0)
 		end
 	end
 
@@ -58,11 +59,20 @@ function GuardianOrb:OnUpdate(dt)
 
 	for i = 1, #self.orbs do
 		local orb = self.orbs[i]
+		local orbPos = orb:GetPosition()
 		local targetPoint = getTargetPoint(self.angle, instPos, i - 1)
 
-		local angle = aipGetAngle(orb:GetPosition(), targetPoint)
+		local angle = aipGetAngle(orbPos, targetPoint)
 		orb.Transform:SetRotation(angle)
 		orb:FacePoint(targetPoint)
+
+		-- 根据玩家间距调整速度
+		local sq = distsq(orbPos.x, orbPos.z, instPos.x, instPos.z)
+		if sq > DISTANCE_FAST * DISTANCE_FAST then
+			orb.Physics:SetMotorVel(ORB_SPEED_FAST, 0, 0) -- 太远了就要追上去
+		else
+			orb.Physics:SetMotorVel(ORB_SPEED, 0, 0) -- 在附近则慢慢来
+		end
 	end
 end
 
