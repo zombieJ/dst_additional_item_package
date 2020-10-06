6cbbe2d16e86d79e3ac90013c53976d497c85a02
jilin <smith3816@gmail.com>
2020-10-06 21:26:26 +0800

    re-recipe

--- scripts/prefabs/aip_glass_chest.lua
+++ scripts/prefabs/aip_glass_chest.lua
@@ -30,7 +30,7 @@ STRINGS.CHARACTERS.GENERIC.DESCRIBE.AIP_GLASS_CHEST = LANG.DESC
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.AIP_GLASS_CHEST_DISABLED = LANG.DISABLED
 
 -- 配方
-local aip_glass_chest = Recipe("aip_glass_chest", {Ingredient("log", 1)}, RECIPETABS.MAGIC, TECH.MAGIC_TWO, "aip_glass_chest_placer")
+local aip_glass_chest = Recipe("aip_glass_chest", { Ingredient("moonglass", 3), Ingredient("nightmarefuel", 1), Ingredient("plantmeat", 1) }, RECIPETABS.MAGIC, TECH.MAGIC_TWO, "aip_glass_chest_placer")
 aip_glass_chest.atlas = "images/inventoryimages/aip_glass_chest.xml"
 
 ---------------------------------- 资源 ----------------------------------
