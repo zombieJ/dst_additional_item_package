a874252c5f5fc02771ce66080d3a4d202da69043
jilin <smith3816@gmail.com>
2021-09-11 22:21:47 +0800

    feat: rubik assets

---
+++ scripts/components/aipc_rubik.lua
@@ -0,0 +1,73 @@
+------------------------------- 方法 -------------------------------
+local FX_OFFSET = 2
+local FX_HEIGHT = 5
+
+local function getPos(idx)
+	local mathIdx = idx - 1
+	local y = math.floor(mathIdx / 9)
+	local x = math.floor(math.mod(mathIdx, 9) / 3)
+	local z = math.mod(mathIdx, 3)
+
+	return x - 1, y - 1, z - 1
+end
+
+------------------------------- 组件 -------------------------------
+local Rubik = Class(function(self, inst)
+	self.inst = inst
+
+	self.start = false
+	self.fxs = {}
+
+	self.matrix = {}
+	local colors = {"red","green","blue"}
+	for y = 0, 2 do
+		local color = colors[y + 1]
+
+		for x = 0, 2 do
+			for z = 0, 2 do
+				local idx = 1 + z + x * 3 + y * 9
+				self.matrix[idx] = color
+			end
+		end
+	end
+end)
+
+function Rubik:SyncPos()
+	for i, fx in ipairs(self.fxs) do
+		local ox, oy, oz = getPos(i)
+
+		local scaleOffset = 1 - (oy - 1) / 6
+		
+		fx.Transform:SetPosition(
+			ox * FX_OFFSET * scaleOffset,
+			oy * FX_OFFSET + FX_HEIGHT,
+			oz * FX_OFFSET * scaleOffset
+		)
+	end
+end
+
+function Rubik:Start()
+	self.start = true
+
+	if #self.fxs == 0 then
+		for i, color in ipairs(self.matrix) do
+			local fx = SpawnPrefab("aip_rubik_fire_"..color)
+			self.inst:AddChild(fx)
+			self.fxs[i] = fx
+		end
+	end
+
+	self:SyncPos()
+end
+
+function Rubik:Stop()
+	self.start = false
+
+	for i, fx in ipairs(self.fxs) do
+		fx:Remove()
+	end
+
+	self.fxs = {}
+end
+
+return Rubik
--- scripts/prefabs/aip_rubik.lua
+++ scripts/prefabs/aip_rubik.lua
@@ -19,8 +19,7 @@ STRINGS.CHARACTERS.GENERIC.DESCRIBE.AIP_RUBIK = LANG.DESC
 
 -- 资源
 local assets = {
-    Asset("ANIM", "anim/aip_22_fish.zip"),
-	Asset("ATLAS", "images/inventoryimages/aip_22_fish.xml"),
+    Asset("ANIM", "anim/aip_rubik.zip"),
 }
 
 local prefabs = {
@@ -29,7 +28,42 @@ local prefabs = {
 	"aip_rubik_fire_red",
 }
 
-------------------------------- 事件 -------------------------------
+------------------------------- 燃烧 -------------------------------
+local function onextinguish(inst)
+    if inst.components.fueled ~= nil then
+        inst.components.fueled:InitializeFuelLevel(0)
+    end
+	inst:RemoveTag("shadow_fire")
+	inst.components.aipc_rubik:Stop()
+end
+
+local function onignite(inst)
+	inst:AddTag("shadow_fire")
+	inst.components.aipc_rubik:Start()
+end
+
+------------------------------- 燃料 -------------------------------
+local function ontakefuel(inst)
+    inst.SoundEmitter:PlaySound("dontstarve/common/nightmareAddFuel")
+end
+
+local function onupdatefueled(inst)
+    if inst.components.burnable ~= nil then
+        inst.components.burnable:SetFXLevel(inst.components.fueled:GetCurrentSection(), inst.components.fueled:GetSectionPercent())
+    end
+end
+
+local function onfuelchange(newsection, oldsection, inst)
+    if newsection <= 0 then
+        inst.components.burnable:Extinguish()
+    else
+        if not inst.components.burnable:IsBurning() then
+            inst.components.burnable:Ignite()
+        end
+
+        inst.components.burnable:SetFXLevel(newsection, inst.components.fueled:GetSectionPercent())
+    end
+end
 
 ------------------------------- 魔方 -------------------------------
 local function CreateFire(inst, colorName, x, y, z)
@@ -66,13 +100,16 @@ local function fn()
 
     inst.entity:AddTransform()
     inst.entity:AddAnimState()
+	inst.entity:AddSoundEmitter()
     inst.entity:AddNetwork()
 
-    MakeInventoryPhysics(inst)
+    MakeObstaclePhysics(inst, .2)
 
-    inst.AnimState:SetBank("aip_22_fish")
-    inst.AnimState:SetBuild("aip_22_fish")
+    inst.AnimState:SetBank("aip_rubik")
+    inst.AnimState:SetBuild("aip_rubik")
     inst.AnimState:PlayAnimation("idle")
+
+	inst:AddTag("wildfireprotected")
 
     inst.entity:SetPristine()
 
@@ -81,12 +118,27 @@ local function fn()
     end
 
     inst:AddComponent("inspectable")
-    
-	inst:AddComponent("inventoryitem")
-	inst.components.inventoryitem.atlasname = "images/inventoryimages/aip_22_fish.xml"
-	inst.components.inventoryitem.imagename = "aip_22_fish"
 
-	initRubik(inst)
+	-- 可以点燃
+	inst:AddComponent("burnable")
+    inst.components.burnable:AddBurnFX("nightlight_flame", Vector3(0, 0, 0), "fire_marker")
+    inst.components.burnable.canlight = false
+    inst:ListenForEvent("onextinguish", onextinguish)
+	inst:ListenForEvent("onignite", onignite)
+
+	-- 使用燃料
+	inst:AddComponent("fueled")
+    inst.components.fueled.maxfuel = TUNING.NIGHTLIGHT_FUEL_MAX
+    inst.components.fueled.accepting = true
+    inst.components.fueled.fueltype = FUELTYPE.NIGHTMARE
+    inst.components.fueled:SetSections(4)
+    inst.components.fueled:SetTakeFuelFn(ontakefuel)
+    inst.components.fueled:SetUpdateFn(onupdatefueled)
+    inst.components.fueled:SetSectionCallback(onfuelchange)
+    inst.components.fueled:InitializeFuelLevel(0)
+
+	-- initRubik(inst)
+	inst:AddComponent("aipc_rubik")
 
 	return inst
 end
