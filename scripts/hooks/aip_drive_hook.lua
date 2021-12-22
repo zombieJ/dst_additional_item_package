35a3886a43daebbc07fcf26e7b194fae6c711134
jilin <smith3816@gmail.com>
2021-12-22 23:34:48 +0800

    add back

--- scripts/hooks/aip_drive_hook.lua
+++ scripts/hooks/aip_drive_hook.lua
@@ -8,6 +8,7 @@ AddStategraphState("wilson", State {
         inst.components.locomotor:Stop()
 		inst.AnimState:PlayAnimation("aip_drive", true)
         inst.AnimState:OverrideSymbol("minecar_down_front", "aip_glass_minecar", "swap_aip_minecar_down_front")
+        inst.AnimState:OverrideSymbol("minecar_down_end", "aip_glass_minecar", "swap_aip_minecar_down_end")
         inst.AnimState:OverrideSymbol("minecar_side", "aip_glass_minecar", "swap_aip_minecar_side")
         inst.AnimState:OverrideSymbol("minecar_up_front", "aip_glass_minecar", "swap_aip_minecar_up_front")
         inst.AnimState:OverrideSymbol("minecar_up_end", "aip_glass_minecar", "swap_aip_minecar_up_end")
