1ee41c0b78c74d9dad886567c75a0419aaff87eb
二货机器人 <smith3816@gmail.com>
2023-01-01 00:53:33 +0800

    drop it

--- scripts/components/aipc_steel_ball.lua
+++ scripts/components/aipc_steel_ball.lua
@@ -1,4 +1,4 @@
-local CD = 0.1
+local CD = 5
 local DIV = 15
 local DIST = 3
 
@@ -6,14 +6,12 @@ local function onHealthDelta(owner, data)
 	local sb = owner.components.aipc_steel_ball
 
 	if sb ~= nil and data ~= nil and data.amount < 0 then
-		local now = os.time()
-
-		if now - sb.lastTransTime < CD then
+		if not sb:CanSteel() then
 			return
 		end
 
 		-- 转移了，重置一下时间
-		sb.lastTransTime = now
+		sb.lastTransTime = os.time()
 
 		local uselessBalls = {}
 		local doEffect = false
@@ -36,6 +34,16 @@ local function onHealthDelta(owner, data)
 						ent.components.workable:WorkedBy(owner, math.ceil(- data.amount / DIV))
 						data.amount = 0
 						doEffect = true
+						aipSpawnPrefab(ent, "aip_shadow_wrapper").DoShow()
+
+						-- 重置 CD
+						sb.canSteel = false
+						sb.syncTask = sb.inst:DoTaskInTime(CD, function()
+							sb.canSteel = true
+							sb.syncTask = nil
+							sb:SyncState()
+						end)
+
 						break
 					end
 				end
@@ -54,6 +62,8 @@ local function onHealthDelta(owner, data)
 				aipReplacePrefab(ball, "aip_steel_ball")
 			)
 		end
+
+		sb:SyncState()
 	end
 end
 
@@ -61,21 +71,56 @@ local SteelBall = Class(function(self, inst)
 	self.inst = inst
 
 	self.balls = {}
-	self.lastTransTime = 0
+	self.aura = nil
+	self.canSteel = nil
+	self.syncTask = nil
 
 	inst:ListenForEvent("aip_healthdelta", onHealthDelta)
 end)
 
+function SteelBall:CanSteel()
+	return self.canSteel
+end
+
+function SteelBall:SyncState()
+	-- 初始化时总是可以
+	if self.canSteel == nil then
+		self.canSteel = true
+	end
+
+	-- 如果没有球则不行
+	if #self.balls == 0 then
+		self.canSteel = false
+	end
+
+	if self.canSteel and self.aura == nil then
+		self.aura = SpawnPrefab("aip_aura_steel")
+		self.aura.AnimState:PlayAnimation("in")
+		self.aura.AnimState:PushAnimation("idle", true)
+		self.inst:AddChild(self.aura)
+	elseif not self.canSteel and self.aura ~= nil then
+		aipSpawnPrefab(self.inst, "aip_shadow_wrapper").DoShow()
+		self.aura:Remove()
+		self.aura = nil
+	end
+end
+
 function SteelBall:Register(ball)
 	table.insert(self.balls, ball)
+	self:SyncState()
 end
 
 function SteelBall:Unregister(ball)
 	aipTableRemove(self.balls, ball)
+	self:SyncState()
 end
 
 function SteelBall:OnRemoveEntity()
 	self.inst:RemoveEventCallback("aip_healthdelta", onHealthDelta)
+	if self.syncTask ~= nil then
+		self.syncTask:Cancel()
+		self.syncTask = nil
+	end
 end
 
 SteelBall.OnRemoveFromEntity = SteelBall.OnRemoveEntity
